9f4458c237ccd46d62b5a64e9e6dfe9b6ebab852
vann <vann@innerfusion.net>
2010-02-03 10:25:10 -0600

    initial commit

---
+++ update_chromium.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASEDIR=http://build.chromium.org/buildbot/snapshots/chromium-rel-mac
+cd ~/tmp/
+
+echo "Downloading number of latest revision"
+REVNUM=`curl -# $BASEDIR/LATEST`
+
+echo "Found latest revision number $REVNUM, starting download"
+
+curl $BASEDIR/$REVNUM/chrome-mac.zip > $REVNUM.zip
+
+echo "Unzipping..."
+unzip $REVNUM.zip 2>&1 > /dev/null
+echo "Done."
+
+echo "Moving to Applications directory..."
+rm -rf /Applications/Chromium.app/
+mv chrome-mac/Chromium.app/ /Applications/
+rm ~/tmp/$REVNUM
+echo "Done, update successful"
+
